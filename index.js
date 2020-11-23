9290e233519066ca21949d525adc87a09a2d4e79
soma sekhar novudu <somasekharnovudu@somas-MacBook-Pro.local>
2020-11-23 20:40:23 +0530

    items added

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require('express');
 const app = express();
 const data = require('./data.json');
 
-app.get('/data', (req, resp) => {
-    resp.send(data)
+app.get('/data/:name', (req, resp) => {
+    const { name } = req.params;
+    const response = data.find((dataObj) => dataObj.id === name);
+    resp.send(response.items)
 })
 app.listen(4000, () => {
     console.log('=====listening on server')
